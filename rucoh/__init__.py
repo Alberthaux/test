d3c5880440b1e39a25f294e283e309cff781b9f3
Alice Berthaux <alice.berthaux@airbnb.com>
2022-09-12 14:20:05 -0700

    rename

--- rucoh/__init__.py
+++ rucoh/__init__.py
@@ -4,10 +4,4 @@ import os
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
-app.config.from_mapping(
-        SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev_key'
-    )
-
-from rucoh import routes
-from rucoh import util
 
