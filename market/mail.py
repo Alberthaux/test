89ad4276e3478c55beff20f3358a718f22e9aa0d
Alice Berthaux <alice.berthaux@airbnb.com>
2022-09-12 02:21:50 -0700

    tests

---
+++ market/mail.py
@@ -0,0 +1,23 @@
+from market import app
+from flask_mail import Mail, Message
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'berthaux.alice@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
+mail = Mail(app)
+my_email=app.config['MAIL_USERNAME']
+
+
+def send_message(
+        user_name,
+        user_email,
+        message
+):
+    msg = Message(
+        sender=my_email,
+        recipients=[user_email])
+    msg.body = f'Message from {user_name}: ' + message
+    #mail.send(msg)
---
+++ market/util.py
@@ -0,0 +1,10 @@
+import glob
+
+
+def get_gallery(page):
+    dir_list = glob.glob(f'market/static/photos/{page}/*')
+    dir_list = [
+        f'../static/photos/{page}/' + d.split('/')[-1]
+        for d in dir_list
+    ]
+    return dir_list
