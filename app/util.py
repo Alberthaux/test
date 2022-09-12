eb0a3e9b578a5fc39ed5e873a3a484aaffe6534b
Alice Berthaux <alice.berthaux@airbnb.com>
2022-09-12 14:38:10 -0700

    rename

--- app/util.py
+++ app/util.py
@@ -2,7 +2,7 @@ import glob
 
 
 def get_gallery(page):
-    dir_list = glob.glob(f'static/photos/{page}/*')
+    dir_list = glob.glob(f'/static/photos/{page}/*')
     dir_list = [
         f'../static/photos/{page}/' + d.split('/')[-1]
         for d in dir_list
