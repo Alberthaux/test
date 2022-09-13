b7acfa16a50c9dd9f8f6efa3f433387bb859d5cd
Alice Berthaux <alice.berthaux@airbnb.com>
2022-09-13 02:03:42 -0700

    rename

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap5
+import os
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
-
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 from app import routes
 
 
